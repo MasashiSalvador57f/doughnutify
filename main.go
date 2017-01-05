7032765d4a6332cab11f7d5bdd67c4215bf6a413
MasashiSalvador57f <masashisalvador57f@gmail.com>
2017-01-06 02:28:43 +0900

    static file and render template

--- main.go
+++ main.go
@@ -2,22 +2,68 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-const port = ":9999"
+const (
+	keyCurrentValue = "current_value"
+	keyTargetValue  = "target_value"
+	keyTitle        = "title"
+)
+
+const templatePath = "index.html"
+const defaultPort = 9999
 
 // Index returns rendered html to client :)
-func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	query := r.URL.Query()
+	currentValue := query.Get(keyCurrentValue)
+	targetValue := query.Get(keyTargetValue)
+	percent := getPercent(currentValue, targetValue)
+	title := query.Get(keyTitle)
+
+	variables := map[string]interface{}{
+		"Percent": percent,
+		"Title":   title,
+	}
+	tmpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		// NOTE: Don't panic .... :)
+		panic(err)
+	}
+	err = tmpl.Execute(w, variables)
+	if err != nil {
+		// NOTE: Don't panic :)
+		panic(err)
+	}
+}
+
+func getPercent(currentStr, targetStr string) string {
+	currentValue, _ := strconv.Atoi(currentStr)
+	targetValue, _ := strconv.Atoi(targetStr)
+
+	if targetValue <= 0 {
+		return "0.0"
+	}
+	p := 100 * float32(currentValue) / float32(targetValue)
+	return fmt.Sprintf("%.2f", p)
 }
 
 func main() {
+	port := os.Getenv("DOUGHNUTIFY_PORT")
+	if port == "" {
+		port = ":9999"
+	}
+
 	router := httprouter.New()
 	router.GET("/", Index)
+	router.ServeFiles("/public/*filepath", http.Dir("public"))
 
 	log.Fatal(http.ListenAndServe(port, router))
 }
